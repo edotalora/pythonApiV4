c07b3ee0388de804a802c3bdae23fd5e192fed55
edotalora <edotalora@gmail.com>
2020-10-29 08:43:50 -0500

    Update routes.py
    Se incluye ruta para api de documentos

--- resources/routes.py
+++ resources/routes.py
@@ -2,6 +2,8 @@ from .client import ClientsApi, ClientApi
 
 
 def initialize_routes(api):
-    api.add_resource(ClientsApi, '/api/clients')
+    api.add_resource(ClientsApi, '/api/clients') 
     api.add_resource(ClientApi, '/api/client/<id>')
+    api.add_resources(DocumentoApi, 'api/documentos')
+    api.add_resource(DocumentoApi, '/api/documento/<id>')
 
