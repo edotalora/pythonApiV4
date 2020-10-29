116dba5e18d602268ea97830abba516f73d60437
edotalora <edotalora@gmail.com>
2020-10-29 08:50:18 -0500

    Se incluye archivo de definiciòn para api de documentos.

---
+++ resources/documento.py
@@ -0,0 +1,26 @@
+from flask import Response, request
+from database.models import Documento
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_restful import Resource
+
+
+class DocumentosApi(Resource):
+    def get(self):
+        documentos = Documento.objects().to_json()
+        return Response(documentos, mimetype="application/json", status=200)
+
+    @jwt_required
+    def post(self):
+        body = request.get_json()
+        documento = Documento(**body).save()
+        id = documento.id
+        return {'id': str(id)}, 200
+
+
+class DocumentoApi(Resource):
+    def get(self, id):
+        try:
+            documento = Documento.objects.get(id=id).to_json()
+            return Response(documento, mimetype="application/json", status=200)
+        except Exception as error:
+            return Response(error, status=400, mimetype='application/json')
