683cf7297dade267b31c2d631d870f2d4adfffcd
edotalora <edotalora@gmail.com>
2020-10-29 20:59:03 -0500

    actualizacion documentos API

--- resources/routes.py
+++ resources/routes.py
@@ -5,6 +5,6 @@ from .documento import DocumentosApi, DocumentoApi
 def initialize_routes(api):
     api.add_resource(ClientsApi, '/api/clients') 
     api.add_resource(ClientApi, '/api/client/<id>')
-    api.add_resource(DocumentoApi, '/api/documentos')
+    api.add_resource(DocumentosApi, '/api/documentos')
     api.add_resource(DocumentoApi, '/api/documento/<id>')
 
