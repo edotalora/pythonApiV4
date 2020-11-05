fca276dd4954bdeaec92668c4e49e02a8d036c75
edotalora <edotalora@gmail.com>
2020-11-05 18:32:40 -0500

    Se actualiza identación

--- database/models.py
+++ database/models.py
@@ -10,5 +10,5 @@ class Documento(db.Document):
     doc_id = db.StringField(required=True, unique=True)
     name = db.StringField(required=True)
     tarifa = db.StringField(required=True)
-	userDoc = db.StringField(required=True)
-	tipoDoc = db.StringField(required=True)
+    userDoc = db.StringField(required=True)
+    tipoDoc = db.StringField(required=True)
