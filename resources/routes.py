1029241430c8bb85feb88b607aa3589d451eafed
edotalora <edotalora@gmail.com>
2020-11-05 18:56:48 -0500

    se actualiza la ruta

--- resources/routes.py
+++ resources/routes.py
@@ -7,5 +7,5 @@ def initialize_routes(api):
     api.add_resource(ClientApi, '/api/client/<id>')
     api.add_resource(DocumentosApi, '/api/documentos')
     api.add_resource(DocumentoApi, '/api/documento/<id>')
-    api.add_resource(DocumentosUsuarioApi, '/api/documentos/<docUsuario>')
+    api.add_resource(DocumentosUsuarioApi, '/api/userdocs/<docUsuario>')
 
