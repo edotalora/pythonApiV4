814c7da1add4e59507f99e92d42a5c53623eec6b
edotalora <edotalora@gmail.com>
2020-11-05 19:37:37 -0500

    consultar tarifa update

--- resources/routes.py
+++ resources/routes.py
@@ -8,5 +8,5 @@ def initialize_routes(api):
     api.add_resource(DocumentosApi, '/api/documentos')
     api.add_resource(DocumentoApi, '/api/documento/<id>')
     api.add_resource(DocumentosUsuarioApi, '/api/consultarDocumentosUsuario/<docUsuario>')
-    api.add_resource(DocumentosTarifaApi, '/api/consultatTarifa/<docUsuario>')
+    api.add_resource(DocumentosTarifaApi, '/api/consultatTarifa/<id>')
 
