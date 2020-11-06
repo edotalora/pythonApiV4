6a7b1d4d8aeeaa079041742cb4991dacc3fe77b9
edotalora <edotalora@gmail.com>
2020-11-06 06:58:45 -0500

    se incluye prueba para un documento

---
+++ test/Documento_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from service.documento import Documento
+
+
+class DocumentoTestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_documento = Client('docName', '12345', '500000')
+
+    def test_name_docName(self):
+        expected_name = 'miDocName'
+        self.assertEqual(self.test_client.get_docName(), expected_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    #unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    # python -m unittest discover -s test -p "*_test.py"
+    # https://stackoverflow.com/questions/11241781/python-unittests-in-jenkins
